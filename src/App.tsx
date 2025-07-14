86a2e8d0c12cdba15a5c3b4ccc0d7f5a25e4699e
YutaUra <yuuta3594@gmail.com>
2025-07-14 23:56:51 +0900

    add preact

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export const App = () => {
+  return (
+    <div>
+      <h1>Poker Chip Calculator</h1>
+      <p>Welcome to the Poker Chip Calculator app!</p>
+    </div>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,8 @@
-console.log('Hello, world!');
+import { render } from "preact"
+import { App } from "./App"
+
+const app = document.getElementById("app")
+
+if (!app) throw new Error("#app element not found")
+
+render(<App />, app)
