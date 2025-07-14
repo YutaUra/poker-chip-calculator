a02eab8507fff659bd685a3216442db5ac30b5bc
YutaUra <yuuta3594@gmail.com>
2025-07-14 23:59:39 +0900

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export const App = () => {
   return (
-    <div>
-      <h1>Poker Chip Calculator</h1>
+    <div className="p-5">
+      <h1 className="text-3xl">Poker Chip Calculator</h1>
       <p>Welcome to the Poker Chip Calculator app!</p>
     </div>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import { cloudflare } from "@cloudflare/vite-plugin";
 import preact from "@preact/preset-vite";
+import tailwindcss from '@tailwindcss/vite'
+
 
 export default defineConfig({
-  plugins: [cloudflare(), preact()]
+  plugins: [cloudflare(), preact(), tailwindcss()]
 })
